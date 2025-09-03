d0787290601279e04a92e8ec841481d63629ec59
周超 <1596181478@qq.com>
2025-09-03 16:16:20 +0800

    feat: 加载 blender 导出的城市 gltf 模型、访问加载场景的某些或某个模型设置材质

--- 章节 46 - 案例：智慧城市/02 - 城市或园区三维场景构建方式/03 - 加载 blender 导出的城市 gltf 模型/scene/group.js
+++ 章节 46 - 案例：智慧城市/02 - 城市或园区三维场景构建方式/03 - 加载 blender 导出的城市 gltf 模型/scene/group.js
@@ -5,8 +5,10 @@ import getModelSize from '../util/getModelSize.js';
 // 组（容器）：统一管理多个网格对象
 const group = new Three.Group();
 
+// 创建 GLTF 模型加载器
 const gltfLoader = new GLTFLoader();
 
+// 加载 GLTF 模型
 gltfLoader.load('./model/上海外滩.glb', (gltf) => {
   console.log('size', getModelSize(gltf.scene));
 
--- 章节 46 - 案例：智慧城市/02 - 城市或园区三维场景构建方式/04 - 访问加载场景的某些或某个模型设置材质/scene/group.js
+++ 章节 46 - 案例：智慧城市/02 - 城市或园区三维场景构建方式/04 - 访问加载场景的某些或某个模型设置材质/scene/group.js
@@ -4,8 +4,10 @@ import { GLTFLoader } from '../../../three/examples/jsm/loaders/GLTFLoader.js';
 // 组（容器）：统一管理多个网格对象
 const group = new Three.Group();
 
+// 创建 GLTF 模型加载器
 const gltfLoader = new GLTFLoader();
 
+// 加载 GLTF 模型
 gltfLoader.load('./model/上海外滩.glb', (gltf) => {
   console.log('gltf.scene.children', gltf.scene.children);
 
