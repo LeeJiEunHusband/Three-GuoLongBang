901da6dc73ee60a147498b5a8f0c280ec2412657
周超 <1596181478@qq.com>
2025-09-03 10:18:09 +0800

    feat: 加载 blender 导出的城市 gltf 模型

---
+++ 章节 46 - 案例：智慧城市/02 - 城市或园区三维场景构建方式/03 - 加载 blender 导出的城市 gltf 模型/scene/group.js
@@ -0,0 +1,16 @@
+import * as Three from '../../../three/build/three.module.js';
+import { GLTFLoader } from '../../../three/examples/jsm/loaders/GLTFLoader.js';
+import { getModelSize } from '../util/getModelSize.js';
+
+// 组（容器）：统一管理多个网格对象
+const group = new Three.Group();
+
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load('./model/上海外滩.glb', (gltf) => {
+  console.log('getModelSize', getModelSize(gltf.scene));
+
+  group.add(gltf.scene);
+});
+
+export default group;
---
+++ 章节 46 - 案例：智慧城市/02 - 城市或园区三维场景构建方式/03 - 加载 blender 导出的城市 gltf 模型/util/getModelSize.js
@@ -0,0 +1,14 @@
+import * as Three from '../../../three/build/three.module.js';
+
+// 计算三维模型的包围盒尺寸
+export function getModelSize(object) {
+  // 创建包围盒对象，用来计算三维物体的整体范围
+  const box3 = new Three.Box3();
+  box3.expandByObject(object);
+
+  // 创建三维向量对象，用来存储包围盒的长宽高
+  const vector3 = new Three.Vector3();
+  box3.getSize(vector3);
+
+  return vector3;
+}
